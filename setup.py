d0e1160fbc02c2f22f074a87b5253647bfb0cbcc
Jonghwan Hyeon <hyeon0145@gmail.com>
2016-09-09 03:18:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='overwatch-stats',
     version='0.1.0',
     description='A library to query a player\'s overwatch stats from Battle.net',
-    long_description=long_description,
     url='https://github.com/hyeon0145/overwatch-stats',
     author='Jonghwan Hyeon',
     author_email='hyeon0145@gmail.com',
@@ -20,7 +12,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Games/Entertainment :: First Person Shooters',
-        'Topic :: Software Development :: Build Tools',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
