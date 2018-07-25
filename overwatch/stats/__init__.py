5357f034dbed52dd9176e4b58fdd7bfdcf5777f7
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-07-25 16:10:03 +0900

    Add endorsement

--- overwatch/stats/__init__.py
+++ overwatch/stats/__init__.py
@@ -5,19 +5,19 @@ import lxml.html
 import requests
 
 from .extractors import (
-    extract_level, extract_icon_url, extract_competitive_rank,
-    extract_time_played_ratios, extract_stats
+    extract_level, extract_endorsement, extract_icon_url,
+    extract_competitive_rank, extract_time_played_ratios, extract_stats
 )
 from .ids import OVERALL_CATEGORY_ID, HERO_CATEGORY_IDS
 from .utils import has_played
 
-STATS_URL = 'https://playoverwatch.com/en-us/career/{platform}/{region}/{battle_tag}'
+STATS_URL = 'https://playoverwatch.com/en-us/career/{platform}/{battle_tag}'
 
 AVAILABLE_PLAY_MODES = ('quick', 'competitive')
 
 
-def query(platform, region, battle_tag):
-    url = STATS_URL.format(platform=platform, region=region, battle_tag=battle_tag.replace('#', '-'))
+def query(platform, battle_tag):
+    url = STATS_URL.format(platform=platform, battle_tag=battle_tag.replace('#', '-'))
     response = requests.get(url)
     if response.status_code == 404:
         raise ValueError('cannot find the player {battle_tag}'.format(battle_tag=battle_tag))
@@ -26,6 +26,7 @@ def query(platform, region, battle_tag):
 
     output = {}
     output['level'] = extract_level(tree)
+    output['endorsement'] = extract_endorsement(tree)
     output['icon_url'] = extract_icon_url(tree)
 
     competitive_rank = extract_competitive_rank(tree)
